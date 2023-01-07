10f24ec2f226c4418f52ae8fac5cef57ea758b12
Fresh <93781766+FreshPenguin112@users.noreply.github.com>
2023-01-06 21:37:14 -0600

    Update m.js

--- m.js
+++ m.js
@@ -1,1 +1,2 @@
-const rgx=/(?:(?:^|[-+_*/])(?:\s*-?\d+(\.\d+)?(?:[eE][+-]?\d+)?\s*))+$/;function parse(t){const mathtosolveeeEeE=t;return rgx.test(mathtosolveeeEeE)?Function(`'use strict'; return (${mathtosolveeeEeE})`)():null}parse("{mathinput}");
+const rgx=/(?:(?:^|[-+_*/])(?:\s*-?\d+(\.\d+)?(?:[eE][+-]?\d+)?\s*))+$/;function parse(t){try{const mathtosolveeeEeE=t;return rgx.test(mathtosolveeeEeE)?Function(`'use strict'; return (${mathtosolveeeEeE})`)():undefined}catch{return undefined}}
+parse("{mathinput}")
